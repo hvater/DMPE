e623ee8865c82821ce5522041a1a425cbbc89b12
Hendrik Vater <vater@lea.uni-paderborn.de>
2024-10-15 17:44:26 +0200

    fix GOATS stepping for PMSM

--- dmpe/related_work/np_reimpl/env_utils.py
+++ dmpe/related_work/np_reimpl/env_utils.py
@@ -5,8 +5,11 @@ def simulate_ahead_with_env(env, obs, state, actions):
     observations = []
     observations.append(obs)
 
+    print(actions.shape)
+    print(actions[0, :][None, :].shape)
+
     for i in range(actions.shape[0]):
-        obs, _, _, _, state = env.step(state, actions[i, :], env.env_properties)
+        obs, state = env.vmap_step(state, actions[i, :][None, :])
         observations.append(obs)
 
     return np.vstack(observations), state
