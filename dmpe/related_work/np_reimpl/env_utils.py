f59cac09174808604b54f7b71aa38d2dcd922064
Hendrik Vater <vater@lea.uni-paderborn.de>
2024-10-21 18:39:12 +0200

    fix for new excenvs interface

--- dmpe/related_work/np_reimpl/env_utils.py
+++ dmpe/related_work/np_reimpl/env_utils.py
@@ -5,11 +5,8 @@ def simulate_ahead_with_env(env, obs, state, actions):
     observations = []
     observations.append(obs)
 
-    print(actions.shape)
-    print(actions[0, :][None, :].shape)
-
     for i in range(actions.shape[0]):
-        obs, state = env.vmap_step(state, actions[i, :][None, :])
+        obs, state = env.step(state, actions[i, :], env.env_properties)
         observations.append(obs)
 
     return np.vstack(observations), state
