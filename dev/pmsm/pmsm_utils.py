fa663f60db5015e363dd3bee8eef0be5bc317a9b
Hendrik Vater <vater@lea.uni-paderborn.de>
2025-03-03 14:29:27 +0100

    implemented proper voltage input constraints

--- dev/pmsm/pmsm_utils.py
+++ dev/pmsm/pmsm_utils.py
@@ -53,7 +53,17 @@ class ExcitingPMSM(PMSM):
 
 def PMSM_penalty(env, observations, actions, penalty_order=2):
 
-    action_penalty = soft_penalty(actions, a_max=1, penalty_order=penalty_order)
+    # action_penalty = soft_penalty(actions, a_max=0.8660254, penalty_order=penalty_order)
+    # action_penalty = soft_penalty(actions, a_max=1, penalty_order=penalty_order)
+
+    if actions is None:
+        action_penalty = 0
+    else:
+        physical_u_d = env.env_properties.action_normalizations.u_d.denormalize(actions[..., 0]) / 400
+        physical_u_q = env.env_properties.action_normalizations.u_q.denormalize(actions[..., 1]) / 400
+
+        action_penalty = jax.nn.relu(jnp.sqrt(physical_u_d**2 + physical_u_q**2) - 1 / jnp.sqrt(3)) ** penalty_order
+        action_penalty = jnp.sum(action_penalty)
 
     physical_i_d = env.env_properties.physical_normalizations.i_d.denormalize(observations[..., 0])
     physical_i_q = env.env_properties.physical_normalizations.i_q.denormalize(observations[..., 1])
@@ -61,7 +71,7 @@ def PMSM_penalty(env, observations, actions, penalty_order=2):
     a = physical_i_d / jnp.abs(env.env_properties.physical_normalizations.i_d.min)
     b = physical_i_q / jnp.abs(env.env_properties.physical_normalizations.i_q.max)
 
-    obs_penalty = jax.nn.relu(a**2 + b**2 - 0.9) ** penalty_order
+    obs_penalty = jax.nn.relu(jnp.sqrt(a**2 + b**2) - 1) ** penalty_order
     obs_penalty = jnp.sum(obs_penalty)
     i_d_penalty = jnp.sum(jax.nn.relu(a)) ** penalty_order
 
@@ -123,11 +133,15 @@ def plot_constraints_induced_voltage(env, physical_i_d, physical_i_q, w_el, satu
     r_s = env.env_properties.static_params.r_s
     psi_p = env.env_properties.static_params.psi_p
 
-    Psi_d = env.pmsm_lut["Psi_d"]
-    Psi_q = env.pmsm_lut["Psi_q"]
-    i_d_vec = np.squeeze(env.pmsm_lut["i_d_vec"])
-    i_q_vec = np.squeeze(env.pmsm_lut["i_q_vec"])
+    if saturated:
+        Psi_d = env.pmsm_lut["Psi_d"][1:-1, 1:-1]
+        Psi_q = env.pmsm_lut["Psi_q"][1:-1, 1:-1]
+        i_d_vec = np.squeeze(env.pmsm_lut["i_d_vec"])
+        i_q_vec = np.squeeze(env.pmsm_lut["i_q_vec"])
     if not saturated:
+
+        # initialize flux matrices and current vectors or something
+
         l_d = env.env_properties.static_params.l_d
         l_q = env.env_properties.static_params.l_q
 
@@ -172,10 +186,10 @@ def plot_constraints_induced_voltage(env, physical_i_d, physical_i_q, w_el, satu
 
     plt.scatter(jnp.squeeze(physical_i_d), jnp.squeeze(physical_i_q), s=1)
 
-    umult = 2 / 3
-    plt.contour(Id, Iq, I, levels=[250, 250 + 1e-6], colors="r", linewidths=2)
+    umult = 1 / jnp.sqrt(3)  # 2 / 3  # * jnp.sqrt(2)
+    plt.contour(Id, Iq, I, levels=[250], colors="r", linewidths=2)
     # plt.contour(Id, Iq, T, linestyles='dashed', colors='k')
-    plt.contour(Id, Iq, U, levels=[udc * umult, (1 + 1e-6) * udc * umult], colors="b", linewidths=2)
+    plt.contour(Id, Iq, U, levels=[udc * umult], colors="b", linewidths=2)
 
     plt.ylim(-350, 350)
 
