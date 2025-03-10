fa9ae53a483e4d6ba2cf96303ce4813bf4589475
Hendrik Vater <vater@lea.uni-paderborn.de>
2025-03-10 10:20:03 +0100

    updated script for multiple exp runs

--- eval/scripts/pmsm/run_multiple.py
+++ eval/scripts/pmsm/run_multiple.py
@@ -1,12 +1,33 @@
+import jax
+import argparse
 import eval_dmpe
+import eval_goats
 
 
-rpms = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000]
-model_names = ["RLS", "NODE", "PM"]
-consider_actions = [False, True]
+rpms = [2000, 3000, 5000, 6000, 7000, 9000]
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run DMPE on the PMSM environment.")
+    parser.add_argument("--DMPE", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--iGOATS", action=argparse.BooleanOptionalAction)
 
-for consider_action in consider_actions:
-    for model_name in model_names:
+    args = parser.parse_args()
+
+    if args.DMPE:
+        model_names = ["RLS", "NODE", "PM"]
+        consider_actions = [False, True]
+
+        for consider_action in consider_actions:
+            for model_name in model_names:
+                for rpm in rpms:
+                    eval_dmpe.main(rpm, model_name, consider_action)
+
+    elif args.iGOATS:
+        jax.config.update("jax_platform_name", "cpu")
+
+        consider_action = True
         for rpm in rpms:
-            eval_dmpe.main(rpm, model_name, consider_action)
+            eval_goats.main(rpm, consider_action)
+
+    else:
+        raise ValueError("Please specify either DMPE or iGOATS")
